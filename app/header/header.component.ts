60f225004e96958dcee9f4e27a6d63757be5aeac
ademkac <32670928+ademkac@users.noreply.github.com>
2020-05-25 23:23:02 +0200

    Add files via upload

---
+++ app/header/header.component.ts
@@ -0,0 +1,50 @@
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Subscription } from 'rxjs';
+
+import { DataStorageService } from '../shared/data-storage.service';
+import { AuthService } from '../auth/auth.service';
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.css']
+})
+export class HeaderComponent implements OnInit, OnDestroy  {
+  collapsed = true;
+  isAuthenticated = false;
+  private userSub: Subscription;
+  isAdmin:string = '';
+
+constructor(
+  private dataStorageService: DataStorageService,
+  private authService: AuthService
+  ){}
+
+
+  ngOnInit(){
+    this.userSub = this.authService.user.subscribe(user => {
+      this.isAuthenticated = !!user;
+      
+      console.log(!user);
+      console.log(!!user);
+    });
+     this.isAdmin = this.authService.getAdmin(); 
+  }
+
+  onSaveData(){
+    this.dataStorageService.storePhones();
+  }
+
+  onFetchData(){
+    this.dataStorageService.fetchPhones().subscribe();
+  }
+
+  onLogout() {
+    this.authService.logout();
+  }
+
+  ngOnDestroy(){
+    this.userSub.unsubscribe();
+  }
+
+}
